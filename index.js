5852b3ae7a6243f8afd16f95c6344518405a19f3
yazar-salim <syazar222@gmail.com>
2025-01-09 10:17:13 +0530

    second commit added new problems

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // display the count of each word
 // const sentence= "js python js python" ;
 // output={};
-// sentence.split(' ').forEach((str)=>output.hasOwnProperty(str)?output[str]+=1:output[str])
+// sentence.split(' ').forEach((str)=>output.hasOwnProperty(str)?output[str]+=1:output[str]=1)
 // console.log(output);
 
 // Use Object.entries() to iterate through an object and log each key-value pair.
@@ -38,7 +38,7 @@
 // display the count of each word
 // const sentence = "Hello world" ;
 // output={}
-// sentence.split(' ').forEach((word)=>word in output?output[word]+=1:output[word])
+// sentence.split(' ').forEach((word)=>word in output?output[word]+=1:output[word]=1)
 // console.log(output);
 
 // const company = {
@@ -85,3 +85,75 @@
 
 // let square=Object.values(objects);
 // console.log(square.map((a)=>a**2));
+
+
+// const products = [
+//     { pid: 100, pName: 'Apple', brand: '5G', price: 120000, display: 'OLED' },
+//     { pid: 101, pName: 'Samsung', brand: '4G', price: 90000, display: 'AMOLED' },
+//     { pid: 102, pName: 'OnePlus', brand: '4G', price: 75000, display: 'Fluid AMOLED' },
+//     { pid: 103, pName: 'Pixel', brand: '3G', price: 85000, display: 'OLED' },
+//     { pid: 104, pName: 'Xiaomi', brand: '3G', price: 40000, display: 'Super AMOLED' }
+// ];
+
+// print product name only
+// products.forEach((obj)=>{
+//     console.log(obj["pName"]);
+    
+// })
+
+
+
+// print all mobile details whose display is lcd : 
+
+// products.forEach((obj)=>{
+//     if(obj["display"]==="OLED"){
+//         console.log(obj);
+//     }
+// })
+
+// print 3g mobile phone names :
+// products.forEach((obj)=>{
+//     if(obj["brand"]==='3G'){
+//         console.log(obj["pName"]);
+        
+//     }
+// })
+
+// sort mobile based on price
+
+// console.log(products.sort((a,b)=>a["price"]-b["price"]));
+
+
+// print costly mobile price
+
+    // let cost=products.reduce((a,b)=>a['price']>b['price']?a:b)
+    // console.log(cost);
+
+
+// print low cost mobile
+// let lowCost=products.reduce((a,b)=>a['price']<b['price']?a:b)
+// console.log(lowCost);
+
+
+
+//Create a word and in that word if 'a' occurs then replace it with #
+// let str="aabjfdijnhinaainjsainj";
+// console.log(str.replaceAll('a','#'));
+
+
+//Sort an array of words alphabetically
+// let array=["yazar","abhijith","sarath","archa","preiztly","amal"];
+// console.log(array.sort());
+
+
+// Count the occurrence of each character in a string (ignoring spaces).
+
+// let str="kskkssdjfi siefhhhsffis sifbbbddlsllvbbjebis";
+// output={}
+// str.split('').forEach((obj)=>{
+//     if(obj!=' '){
+//         output.hasOwnProperty(obj)?output[obj]+=1:output[obj]=1
+//     }
+// });
+// console.log(output);
+
